7e0b600e980a69114517e4cfd6b55891c9409a6d
misostack <misostack.com@gmail.com>
2025-04-27 19:02:47 +0700

    add login and download

--- express_001/server.js
+++ express_001/server.js
@@ -9,8 +9,126 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "html");
 
-app.get("/", (req, res) => {
+// session
+var hash = require("pbkdf2-password")();
+var session = require("express-session");
+
+// path to where the files are stored on disk
+var FILES_DIR = path.join(__dirname, "files");
+
+// middleware
+app.use(express.urlencoded());
+app.use(
+  session({
+    resave: false, // don't save session if unmodified
+    saveUninitialized: false, // don't create session until something stored
+    secret: "shhhh, very secret",
+  })
+);
+
+// Session-persisted message middleware
+
+app.use(function (req, res, next) {
+  var err = req.session.error;
+  var msg = req.session.success;
+  delete req.session.error;
+  delete req.session.success;
+  res.locals.message = "";
+  if (err) res.locals.message = '<p class="msg error">' + err + "</p>";
+  if (msg) res.locals.message = '<p class="msg success">' + msg + "</p>";
+  next();
+});
+
+// dummy database
+
+var users = {
+  tj: { name: "tj" },
+};
+
+// when you create a user, generate a salt
+// and hash the password ('foobar' is the pass here)
+
+hash({ password: "foobar" }, function (err, pass, salt, hash) {
+  if (err) throw err;
+  // store the salt & hash in the "db"
+  users.tj.salt = salt;
+  users.tj.hash = hash;
+});
+
+// Authenticate using our plain-object database of doom!
+
+function authenticate(name, pass, fn) {
+  if (!module.parent) console.log("authenticating %s:%s", name, pass);
+  var user = users[name];
+  // query the db for the given username
+  if (!user) return fn(null, null);
+  // apply the same algorithm to the POSTed password, applying
+  // the hash against the pass / salt, if there is a match we
+  // found the user
+  hash({ password: pass, salt: user.salt }, function (err, pass, salt, hash) {
+    if (err) return fn(err);
+    if (hash === user.hash) return fn(null, user);
+    fn(null, null);
+  });
+}
+
+function restrict(req, res, next) {
+  if (req.session.user) {
+    next();
+  } else {
+    req.session.error = "Access denied!";
+    res.redirect("/login");
+  }
+}
+
+app.get("/", restrict, (req, res) => {
   res.render("index", { title: "Hello World" });
+});
+
+app.get("/logout", function (req, res) {
+  // destroy the user's session to log them out
+  // will be re-created next request
+  req.session.destroy(function () {
+    res.redirect("/");
+  });
+});
+
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+
+app.post("/login", function (req, res, next) {
+  if (!req.body) return res.sendStatus(400);
+  authenticate(req.body.username, req.body.password, function (err, user) {
+    if (err) return next(err);
+    if (user) {
+      // Regenerate session when signing in
+      // to prevent fixation
+      req.session.regenerate(function () {
+        // Store the user's primary key
+        // in the session store to be retrieved,
+        // or in this case the entire user object
+        req.session.user = user;
+        res.redirect("/");
+      });
+    } else {
+      req.session.error =
+        "Authentication failed, please check your " +
+        " username and password." +
+        ' (use "tj" and "foobar")';
+      res.redirect("/login");
+    }
+  });
+});
+
+app.get("/files/*file", function (req, res, next) {
+  res.download(req.params.file.join("/"), { root: FILES_DIR }, function (err) {
+    if (!err) return; // file sent
+    if (err.status !== 404) return next(err); // non-404 error
+    // file for download not found
+    res.statusCode = 404;
+    res.send("Cant find that file, sorry!");
+  });
 });
 
 app.listen(port, () => {
